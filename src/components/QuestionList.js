74a212b706b87c0f2fd93aeb643cb75c6e8f143e
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-05-12 21:29:37 -0400

    commit tree 43aad3c44342d65c6cd7a59afea3d7dfdf4f8820

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((items) => setQuestions(items));
+  }, []);
+  const questionItems = questions.map((q) => {
+    <QuestionItem key={q.id} question={q} />;
+  });
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
